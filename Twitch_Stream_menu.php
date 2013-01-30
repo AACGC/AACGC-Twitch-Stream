fe8ed9648de97b4fb890a99bfbe0b5aabc558e5e
James <admin@aacgc.com>
2013-01-29 21:47:35 -0500

    v1.1 unchanged

---
+++ Twitch_Stream_menu.php
@@ -0,0 +1,69 @@
+<?php
+	
+//-----------------------------------------------------------------------------------------------------------+
+include_lan(e_PLUGIN."aacgc_twitchstream/languages/".e_LANGUAGE.".php");
+//-----------------------------------------------------------------------------------------------------------+
+if($pref['twitch_theme'] == "1"){
+$themea = "forumheader3";
+$themeb = "indent";}
+else
+{$themea = "";
+$themeb = "";}
+//------------------------------------------------------
+$twitchstream_title .= $pref['twitch_menutitle'];
+//------------------------------------------------------
+
+$twitchstream_text .= "".$tp->toHTML($pref['twitch_mheader'], TRUE)."";
+
+if($pref['twitch_menuheight'] != "auto")
+{$twitchstream_text .= "<div style='width:100%; height:".$pref['twitch_menuheight']."; overflow:auto'>";}
+
+$twitchstream_text .= "
+<table style='width:100%' class=''>
+	<tr>
+		<td style='width:100%' class='".$themea."'>".TWS_02."</td>
+		<td style='width:0%' class='".$themea."'>".TWS_07."</td>
+	</tr>";
+
+$sql->mySQLresult = @mysql_query("select * from ".MPREFIX."user_extended x, ".MPREFIX."user u where u.user_id=x.user_extended_id order by u.user_name ASC;");
+while($row = $sql ->db_Fetch()){
+$twitchname = $row['user_twitchname'];
+if($twitchname != ""){
+$username = $row['user_name'];
+$userid = $row['user_id'];
+
+$mycurl = curl_init(); 
+curl_setopt ($mycurl, CURLOPT_HEADER, 0); 
+curl_setopt ($mycurl, CURLOPT_RETURNTRANSFER, 1);  
+$url = "http://api.justin.tv/api/stream/list.json?channel=".$twitchname;  
+curl_setopt ($mycurl, CURLOPT_URL, $url); 
+$response =  curl_exec($mycurl);  
+$results = json_decode($response); 
+if(!$results)
+{$status = "".TWS_06."";
+$view = "<a href='".e_PLUGIN."aacgc_twitchstream/Twitch_Stream.php?".$twitchname.".".$userid."'><img height='25px' src='".e_PLUGIN."aacgc_twitchstream/images/offline.png' border='0' alt='".$status."' /></a>";}
+else
+{$status = "".TWS_08."";
+$view = "<a href='".e_PLUGIN."aacgc_twitchstream/Twitch_Stream.php?".$twitchname.".".$userid."'><img height='25px' src='".e_PLUGIN."aacgc_twitchstream/images/online.png' border='0' alt='".$status."' /></a>";}
+
+$twitchstream_text .= "
+	<tr>
+		<td class='".$themeb."'><a href='".e_BASE."user.php?id.".$userid."'><b>".$username."</b></a><br/>(".$twitchname.")</td>
+		<td class='".$themeb."' style='text-align:center'>".$view."<br/>".$status."</td>
+	</tr>";
+curl_close($mycurl);	
+	}
+
+}
+
+$twitchstream_text .= "</table>";
+
+if($pref['twitch_menuheight'] != "auto")
+{$twitchstream_text .= "</div>";}
+
+//------------------------------------------------------
+
+$ns->tablerender($twitchstream_title, $twitchstream_text);
+
+//------------------------------------------------------
+?>
---
+++ languages/English.php
@@ -0,0 +1,37 @@
+<?php
+
+define('ATWS_01', 'Main');
+define('ATWS_02', 'Settings');
+define('ATWS_03', 'Check For Updates');
+define('ATWS_04', 'Settings Saved');
+define('ATWS_05', 'Main Settings');
+define('ATWS_06', 'Page Title');
+define('ATWS_07', 'Menu Title');
+define('ATWS_08', 'Use Theme Style.css');
+define('ATWS_09', 'Save Settings');
+define('ATWS_10', 'Page Details');
+define('ATWS_11', 'Menu Height');
+define('ATWS_12', '<i>max height in px, uses scrolling layer or set to auto for no scrolling layer</i>');
+define('ATWS_13', 'Show status in profiles');
+define('ATWS_14', 'Show status in forums');
+define('ATWS_15', 'Menu Details');
+
+
+define('TWS_01', 'Twitch Steamers');
+define('TWS_02', 'Username');
+define('TWS_03', 'Twitch Name');
+define('TWS_04', 'View');
+define('TWS_05', 'Return to Steamer List');
+define('TWS_06', '<i>Offline</i>');
+define('TWS_07', 'Status');
+define('TWS_08', 'Live');
+define('TWS_09', 'Stream Details');
+define('TWS_10', 'Category');
+define('TWS_11', 'Game');
+define('TWS_12', 'Title');
+define('TWS_13', 'Time Zone');
+define('TWS_14', 'Up Time');
+define('TWS_15', 'No stream information available.');
+
+
+?>
